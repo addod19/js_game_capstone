e459a6c5f66aefcb7ac3832dfa7450706e6596ea
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-05-23 13:03:16 +0000

    create options scene

---
+++ src/scenes/OptionsScene.js
@@ -0,0 +1,45 @@
+import 'phaser';
+import config from '../config';
+import Button from '../Objects/Button';
+
+export default class OptionsScene extends Phaser.Scene {
+  constructor() {
+    super('Options');
+  }
+
+  create() {
+    this.model = this.sys.game.globals.model;
+
+    this.text = this.add.text(300, 50, 'Options', { fontSize: 40 });
+    this.musicButton = this.add.image(200, 100, 'checkedBox');
+    this.musicText = this.add.text(250, 100, 'Music Enabled', { fontSize: 24 });
+
+    this.musicButton.setInteractive();
+
+    this.musicButton.on('pointerdown', () => {
+      this.model.musicOn = !this.model.musicOn;
+      this.updateAudio();
+    });
+
+
+    
+    this.gameButton = new Button(this, config.width / 2, config.height / 2 + 100, 'Button1', 'Button2', 'Back', 'Title');
+
+    this.updateAudio();
+  }
+
+  updateAudio() {
+    if (this.model.musicOn === false) {
+      this.musicButton.setTexture('box');
+      this.sys.game.globals.bgMusic.stop();
+      this.model.bgMusicPlaying = false;
+    } else {
+      this.musicButton.setTexture('checkedBox');
+      if (this.model.bgMusicPlaying === false) {
+        this.sys.game.globals.bgMusic.play();
+        this.model.bgMusicPlaying = true;
+      }
+    }
+
+  }
+}
