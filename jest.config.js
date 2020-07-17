2e5a14b3a6c69d591a6fe41511050f514b5df6e0
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-07-17 06:52:21 +0000

    fix linter errors

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   verbose: true,
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleDirectories: ['node_modules', 'src'],
   transform: {
     '^.+\\.js?$': 'babel-jest',
   },
